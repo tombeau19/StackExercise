dec3e83ed4d9ffdf471b7115d48906843f716348
tombeau19 <thomas.beauregard@gmail.com>
2021-03-30 11:30:53 -0400

    first try no google

--- StackExercise/Program.cs
+++ StackExercise/Program.cs
@@ -6,7 +6,14 @@ namespace StackExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
         }
     }
 }
---
+++ StackExercise/Stack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackExercise
+{
+    public class Stack
+    {
+        public List<object> StackObjects { get; private set; }
+
+        public Stack()
+        {
+            StackObjects = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("cannot add null");
+
+            StackObjects.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (StackObjects.Count == 0)
+                throw new InvalidOperationException("no items in stack");
+
+            var last = StackObjects.Last();
+            StackObjects.RemoveAt(StackObjects.Count - 1);
+            return last;
+        }
+
+        public void Clear()
+        {
+            StackObjects.Clear();
+        }
+    }
+}
