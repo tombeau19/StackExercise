ebe5e381765a4780d1bee6b47d799a4b53083b0c
tombeau19 <thomas.beauregard@gmail.com>
2021-03-30 11:18:54 -0400

    Add project files.

---
+++ StackExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
