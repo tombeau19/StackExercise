13fce6faab789ac66470b8bd48238f129648e752
tombeau19 <thomas.beauregard@gmail.com>
2021-03-30 11:35:39 -0400

    removed constructor and initializing empty list at the field declaration, changed declaration to private field. I had it as a private set so it was already encapsulated

--- StackExercise/Stack.cs
+++ StackExercise/Stack.cs
@@ -6,34 +6,32 @@ namespace StackExercise
 {
     public class Stack
     {
-        public List<object> StackObjects { get; private set; }
-
-        public Stack()
-        {
-            StackObjects = new List<object>();
-        }
+        private List<object> _stackObjects = new List<object>();
 
         public void Push(object obj)
         {
             if (obj == null)
                 throw new InvalidOperationException("cannot add null");
 
-            StackObjects.Add(obj);
+            _stackObjects.Add(obj);
         }
 
         public object Pop()
         {
-            if (StackObjects.Count == 0)
+            if (_stackObjects.Count == 0)
                 throw new InvalidOperationException("no items in stack");
 
-            var last = StackObjects.Last();
-            StackObjects.RemoveAt(StackObjects.Count - 1);
+            var last = _stackObjects.Last();
+            _stackObjects.RemoveAt(_stackObjects.Count - 1);
             return last;
         }
 
         public void Clear()
         {
-            StackObjects.Clear();
+            if(_stackObjects.Count == 0)
+                Console.WriteLine("already cleared");
+
+            _stackObjects.Clear();
         }
     }
 }
